3361289d2670a640149fd8cb9a3845f17c202d6d
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-11-17 08:29:43 +0530

    Welcome to AddressBook

---
+++ src/main/java/AddressBookWorkshop/AddressBookMain.java
@@ -0,0 +1,14 @@
+package AddressBookWorkshop;
+
+import java.util.*;
+import java.io.*;
+
+public class AddressBookMain {
+	public static void welcome() {
+		System.out.println("Welcome to AddressBook");
+	}
+
+	public static void main(String[] args) {
+		welcome();
+	}
+}
